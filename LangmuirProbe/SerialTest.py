2c90414552b519eae49848f0fdc54d978d3ca5bd
James Phillips <jrphill5@users.noreply.github.com>
2018-03-08 13:56:52 -0500

    Created simple TX/RX example script

--- LangmuirProbe/SerialTest.py
+++ LangmuirProbe/SerialTest.py
@@ -1,5 +1,19 @@
-import serial
+import sys, serial
 
-port = serial.Serial("/dev/serial0", baudrate=115200, timeout=3.0)
+# Open serial port object with specified parameters
+ser = serial.Serial(port = "/dev/serial0", baudrate = 115200, parity = serial.PARITY_NONE, stopbits = serial.STOPBITS_ONE, bytesize = serial.EIGHTBITS, timeout = 0)
 
-port.write("Hello World")
+# Check if port is open and, if not, exit
+if ser.is_open:
+    print("Opened port " + ser.port)
+else:
+    print("Error opening port " + ser.port)
+    sys.exit(1)
+
+# Take user input and encode string as bytes before transmit
+cmd = input("Serial command: ")
+ser.write(cmd.encode())
+
+# Read one line of serial data and decode bytes to string
+data = ser.readline()
+print(data.decode())

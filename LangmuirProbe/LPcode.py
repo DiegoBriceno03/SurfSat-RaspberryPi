5a33fde1900e12c835636e78e6abb660294172b0
James Phillips <jrphill5@users.noreply.github.com>
2018-03-16 17:26:18 -0400

    Improved plotting script and produced example data

--- LangmuirProbe/LPcode.py
+++ LangmuirProbe/LPcode.py
@@ -1,24 +1,24 @@
-#SurfSat Pulsed Langmuir Probe Board
+# SurfSat Pulsed Langmuir Probe Board
 
 
-#Pulsed Langmuir Probe 15-pin Micro-D Pinout
-#PIN	Signal	Type	Description	Comments
+# Pulsed Langmuir Probe 15-pin Micro-D Pinout
+# PIN	Signal	Type	Description	Comments
 
-#1	VIN	PWR	+6-28V Supply	-
-#2	VIN	PWR	+6-28V Supply	-
-#3	VBUS	Input	+5V		-
-#4	USB_P	I/O D.	USB Data +	-
-#5	USB_N	I/O D.	USB Data -	-
-#6	RESET	Input	Reset FPGA	3.3 V LVCMOS, active low
-#7	TxD	Output	COM1 Transmit	3.3 V LVCMOS, 4 mA drive, UART
-#8	RxD	Input	COM1 Receive	3.3 V LVCMOS, 4 mA drive, UART
-#9	ENABLE	Input	Board POWER	3.3 V, active high
-#10	GND	GND	GND Reference	-
-#11	GND	GND	GND Reference	-
-#12	GND	GND	GND Reference	-
-#13	GND	GND	GND Reference	-
-#14	SPARE	I/O	Spare GPIO	3.3 V LVCMOS, 4 mA drive
-#15	STATUS	Output	Data Valid	3.3 V LVCMOS, 4 mA drive
+# 1		VIN		PWR		+6-28V Supply	-
+# 2		VIN		PWR		+6-28V Supply	-
+# 3		VBUS	Input	+5V				-
+# 4		USB_P	I/O D.	USB Data +		-
+# 5		USB_N	I/O D.	USB Data -		-
+# 6		RESET	Input	Reset FPGA		3.3 V LVCMOS, active low
+# 7		TxD		Output	COM1 Transmit	3.3 V LVCMOS, 4 mA drive, UART
+# 8		RxD		Input	COM1 Receive	3.3 V LVCMOS, 4 mA drive, UART
+# 9		ENABLE	Input	Board POWER		3.3 V, active high
+# 10	GND		GND		GND Reference	-
+# 11	GND		GND		GND Reference	-
+# 12	GND		GND		GND Reference	-
+# 13	GND		GND		GND Reference	-
+# 14	SPARE	I/O		Spare GPIO		3.3 V LVCMOS, 4 mA drive
+# 15	STATUS	Output	Data Valid		3.3 V LVCMOS, 4 mA drive
 
 import sys
 import serial
@@ -45,7 +45,7 @@ print("Enabling board...")
 GPIO.output(LP_ENABLE_PIN, GPIO.HIGH)
 time.sleep(0.2)
 
-#Defining serial
+# Defining serial
 ser = serial.Serial(
 
 	port="/dev/serial0",
@@ -58,17 +58,17 @@ ser = serial.Serial(
 	)
 
 
-#File.txt to save data
+# File.txt to save data
 saveFile = open('data.txt','w')
 
 
-#Abort program if serial port does not open
+# Abort program if serial port does not open
 if not ser.is_open:
 	#print("Failed to open serial port")
 	sys.exit(1)
 
 
-#Check LP_STATUS_PIN;  if bad status, abort
+# Check LP_STATUS_PIN;  if bad status, abort
 #if GPIO.input(LP_STATUS_PIN) == ???
 #	print("Status check failed, disabling board and aborting program")
 #	sys.stdout.flush()
@@ -76,30 +76,33 @@ if not ser.is_open:
 #	sys.exit(1)
 
 
-#PLP Command Byte - FIRST time sending the byte - START collecting continuous data
-#science - science mode - single waveform - fixed bias - continuous - fast
-#binary(11100101) - hex(E5) - decimal(229)
-#science - science mode - single waveform - swept bias - continuous - fast
-#binary(11100001) - hex(E1)
-commandByte = b'\xE1'
+# PLP Command Byte - FIRST time sending the byte - START collecting continuous data
+# science - science mode - single waveform - fixed bias - continuous - fast
+# binary(11100101) - hex(E5)
+# science - science mode - single waveform - swept bias - continuous - fast
+# binary(11100001) - hex(E1)
+# science - science mode - single waveform - fixed bias - continuous - slow
+# binary(11100100) - hex(E4)
+# science - science mode - single waveform - swept bias - continuous - slow
+# binary(11100000) - hex(E0)
+commandByte = b'\xE0'
 print(commandByte)
 ser.write(commandByte)
 
 print("Saving data to \"data.txt\"")
-#header = ser.read(4)
-#value = 0
-#for i, x in enumerate(header):
-#	value |= (x << (len(header)-i-1)*8)
-#print(header, '{0:x}'.format(value), commandByte)
-#if header[3] is not commandByte:
-#	print('Echoed command byte does not match!')
 
-while True:
+# First four bytes should be header, but is garbage, so ignore.
+header = ser.read(4)
+
+starttime = time.time()
+runtime = 0
+while runtime < 1:
 	try:
+		runtime = time.time() - starttime
 		data = ser.read(4)
-		current = data[0] << 8 | data[1]
-		voltage = data[2] << 8 | data[3]
-		datastr = "{0:x}, {1:x}\n".format(current, voltage)
+		current = data[2] << 8 | data[3]
+		voltage = data[0] << 8 | data[1]
+		datastr = "{0:.6f}, {1:x}, {2:x}\n".format(runtime, voltage, current)
 		if data is not b'':
 			sys.stdout.write(datastr)
 		#save data to file - must cast str() to read list into file, use NO str() if using print()
@@ -109,9 +112,9 @@ while True:
 
 saveFile.close()
 
-#PLP Command Byte - SECOND time sending the byte - STOP collecting continuous  data
-#idle - science mode - single waveform - fixed bias - continuous - fast
-#binary(01100101) - hex(65) - decimal (101)
+# PLP Command Byte - SECOND time sending the byte - STOP collecting continuous  data
+# idle - science mode - single waveform - fixed bias - continuous - fast
+# binary(01100101) - hex(65) - decimal (101)
 
 #idleCommandByte = bytes([0x65])
 idleCommandByte = b'\x65'
--- LangmuirProbe/LPplot.py
+++ LangmuirProbe/LPplot.py
@@ -1,15 +1,27 @@
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 
+runtime = []
 current = []
 voltage = []
 with open('data.txt', 'r') as f:
 	for line in f:
-		c, v = line.split(', ')
+		t, v, c = line.split(', ')
+		runtime.append(float(t))
 		current.append(int(c, 16))
 		voltage.append(int(v, 16))
 
-plt.plot(range(len(current)), current, label="Current")
-plt.plot(range(len(voltage)), voltage, label="Voltage")
+plt.figure(1)
+plt.title("Langmuir Probe Data")
+plt.xlabel("Time (s)")
+plt.plot(runtime, voltage, label="Voltage (arb)")
+plt.plot(runtime, current, label="Current (arb)")
 plt.legend()
-plt.savefig('data.png')
-plt.show()
+plt.savefig('CVvT.png', dpi=200)
+
+plt.figure(2)
+plt.title("Langmuir Probe IV Curve")
+plt.xlabel("Voltage (arb)")
+plt.ylabel("Current (arb)")
+plt.plot(voltage, current, '.')
+plt.savefig('CvV.png', dpi=200)

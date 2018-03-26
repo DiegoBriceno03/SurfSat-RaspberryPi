1d8e76339fb89d87b053f0bae43775e5f3ed33e0
James Phillips <jrphill5@users.noreply.github.com>
2018-03-25 21:49:12 -0400

    Rewrote section of code to locate buffers using loops

--- Picoscope/SimplePicoscopePlot.py
+++ Picoscope/SimplePicoscopePlot.py
@@ -18,21 +18,13 @@ if __name__ == "__main__":
 			state.range = ps.m.Ranges.r5v
 			status = ps.set_channel(channel=channel, state=state)
 			if status == ps.m.pico_num("PICO_OK"):
-				s, index = ps.locate_buffer(channel=channel,
-											samples=samples,
-											segment=0,
-											mode=ps.m.RatioModes.raw,
-											downsample=0)
+				s, index = ps.locate_buffer(channel=channel, samples=samples, segment=0, mode=ps.m.RatioModes.raw, downsample=0)
 				status = ps.collect_segment(segment=0, interval=interval)
 				if status == ps.m.pico_num("PICO_OK"):
 					status, data[channel] = ps.get_buffer_volts(index=index)
 				else: print(ps.m.pico_tag(status))
 			else: print(ps.m.pico_tag(status))
-		print(len(time))
-		plt.plot(time, data[ps.m.Channels.A], label='Channel A')
-		plt.plot(time, data[ps.m.Channels.B], label='Channel B')
-		plt.plot(time, data[ps.m.Channels.C], label='Channel C')
-		plt.plot(time, data[ps.m.Channels.D], label='Channel D')
+			plt.plot(time, data[channel], label='Channel ' + str(channel))
 		plt.title('Picoscope Data')
 		plt.xlabel('Time (ns)')
 		plt.ylabel('Voltage (V)')

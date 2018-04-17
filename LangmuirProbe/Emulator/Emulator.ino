0b146633528ad06b2131368cf35fe0bb8ba80c54
James Phillips <jrphill5@users.noreply.github.com>
2018-04-17 16:19:13 -0400

    Modified program to generate variable data

--- LangmuirProbe/Emulator/Emulator.ino
+++ LangmuirProbe/Emulator/Emulator.ino
@@ -1,21 +1,64 @@
+// Define global boolean to track science vs idle mode
 boolean sciencemode = false;
 
-void setup() {
-  // PLP uses 115200 8N1 UART
-  Serial.begin(115200);
-  pinMode(13, OUTPUT);
+// Define global long integer to track simulated data
+unsigned long data = 0x00000000;
+
+void setup()
+{
+
+	// PLP uses 115200 8N1 UART
+	Serial.begin(115200);
+
+	// Configure onboard LED to indicate science mode
+	pinMode(13, OUTPUT);
+	digitalWrite(13, LOW);
+
 }
 
-void loop() {
-  // PLP sends four bytes every 1 ms in science mode
-  if(sciencemode) Serial.print("abcd");
-  delay(1);
+void loop()
+{
+
+	// PLP sends four bytes every 1 ms in science mode
+	if(sciencemode)
+	{
+
+		// Define character array to store TX data
+		char datastr[4] = {};
+
+		// Update data string with values from integer
+		for( char i = 0; i < 32; i+=8)
+			datastr[i] = (data & (0xFF << i*3)) >> i*3;
+
+		// Transmit the simulated data string
+		Serial.print(data);
+
+		// Increment the simulated data variable
+		data += 0x00200040;
+
+	}
+
+	// Send data at a cadence of 1 ms
+	delay(1);
+
 }
 
-void serialEvent() {
-  char inChar = (char)Serial.read();
-  // If command byte has a 1 in the MSB, enter science mode
-  // Otherwise, enter idle mode and await new command byte
-  sciencemode = (inChar & 0x80);
-  digitalWrite(13, sciencemode ? HIGH : LOW);
+void serialEvent()
+{
+
+	// Read in buffered data when serial interrupt is detected
+	while(Serial.available())
+	{
+
+		char inChar = (char)Serial.read();
+
+		// If command byte has a 1 in the MSB, enter science mode
+		// Otherwise, enter idle mode and await new command byte
+		sciencemode = (inChar & 0x80);
+
+		// Update LED to indicate science mode status
+		digitalWrite(13, sciencemode ? HIGH : LOW);
+
+	}
+
 }

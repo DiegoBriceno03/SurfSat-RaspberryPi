50f1ded797095e7ded8ae5baeba95d4bb2d4d287
James Phillips <jrphill5@users.noreply.github.com>
2018-04-21 09:08:28 -0400

    Completed adding register data for section 8 of datasheet

--- CCDR/SC16IS750.py
+++ CCDR/SC16IS750.py
@@ -12,23 +12,23 @@ REG_LCR       = 0x03 # Line Control Register (R/W)
 REG_MCR       = 0x04 # Modem Control Register (R/W)
 REG_LSR       = 0x05 # Line Status Register (R)
 REG_MSR       = 0x06 # Modem Status Register (R)
-REG_SPR       = 0x07 # Scratchpad Register (R/W)
+REG_SPR       = 0x07 # Scratch Pad Register (R/W)
 REG_TCR       = 0x06 # Transmission Control Register (R/W)
 REG_TLR       = 0x07 # Trigger Level Register (R/W)
 REG_TXLVL     = 0x08 # Transmit FIFO Level Register (R)
 REG_RXLVL     = 0x09 # Receive FIFO Level Register (R)
-REG_IODIR     = 0x0A # I/O Pin Direction Register (R/W)
-REG_IOSTATE   = 0x0B # I/O Pin State Register (R)
-REG_IOINTENA  = 0x0C # I/O Interrupt Enable Register (R/W)
-REG_IOCONTROL = 0x0E # I/O Pin Control Register (R/W)
-REG_EFCR      = 0x0F # Extra Features Register (R/W)
+REG_IODIR     = 0x0A # GPIO Pins Direction Register (R/W)
+REG_IOSTATE   = 0x0B # GPIO Pins State Register (R)
+REG_IOINTENA  = 0x0C # GPIO Interrupt Enable Register (R/W)
+REG_IOCONTROL = 0x0E # GPIO Control Register (R/W)
+REG_EFCR      = 0x0F # Extra Features Control Register (R/W)
 
 # Special Registers (Require LCR[7] = 1 and LCR != 0xBF)
 REG_DLL       = 0x00 # Divisor Latch LSB Register (R/W)
 REG_DLH       = 0x01 # Divisor Latch MSB Register (R/W)
 
 # Enhanced Registers (Require LCR[7] = 1 and LCR = 0xBF)
-REG_EFR       = 0x02 # Enhanced Feature Register (R/W)
+REG_EFR       = 0x02 # Enhanced Features Register (R/W)
 REG_XON1      = 0x04 # XON1 Word Register (R/W)
 REG_XON2      = 0x05 # XON2 Word Register (R/W)
 REG_XOFF1     = 0x06 # XOFF1 Word Register (R/W)
@@ -131,6 +131,64 @@ IIR_GPIO        = 0x30 # Priority 5 # Not available on 740 variant
 IIR_XOFF        = 0x10 # Priority 6
 IIR_CTS_RTS     = 0x20 # Priority 7
 
+# Section 8.11: Enhanced Features Register (EFR)
+# Register only accessible if LCR = 0xBF
+EFR_FLOW_CONTROL_CTS_ENABLE    = 0x01 << 7
+EFR_FLOW_CONTROL_RTS_ENABLE    = 0x01 << 6
+EFR_SPECIAL_CHAR_DETECT_ENABLE = 0x01 << 5
+EFR_ENHANCED_FUNCTIONS_ENABLE  = 0x01 << 4
+EFR_FLOW_CONTROL_TX_NONE       = 0x00
+EFR_FLOW_CONTROL_TX_XON_XOFF_1 = 0x01 << 3
+EFR_FLOW_CONTROL_TX_XON_XOFF_2 = 0x01 << 2
+EFR_FLOW_CONTROL_RX_NONE       = 0x00
+EFR_FLOW_CONTROL_RX_XON_XOFF_1 = 0x01 << 1
+EFR_FLOW_CONTROL_RX_XON_XOFF_2 = 0x01 << 0
+
+# Section 8.12: Division Registers (DLL, DLH)
+# Registers only accessible if LCR[7] = 1 and LCR != 0xBF
+# DLL and DLH can only be set before sleep is enabled (IER[4] = 0)
+
+# Section 8.13: Transmission Control Register (TCR)
+# TCR can only be set when EFR[4] = 1 and MCR[2] = 1
+# TCR[3:0] must be larger than TCR[7:4] (no hardware check to verify)
+# TCR[3:0] and TCR[7:4] define RX FIFO levels to halt and resume TX 
+# Each nybble represents a value from 0 to 60 bytes with granularity of 4 bytes
+
+# Section 8.14: Trigger Level Register (TLR)
+# TLR can only be set when EFR[4] = 1 and MCR[2] = 1
+# If TLR[7:4] or TLR[3:0] is zero, FCR sets associated trigger level
+# Each nybble represents a value from 4 to 60 bytes with granularity of 4 bytes
+# When TLR is used for RX trigger control, FCR[7:6] should be left unset
+
+# Section 8.15: Transmit FIFO Level Register (TXLVL)
+# Reports number of spaces available in TX FIFO from 0x00 (0) to 0x40 (64)
+
+# Section 8.16: Receive FIFO Level Register (RXLVL)
+# Reports number of bytes stored in RX FIFO from 0x00 (0) to 0x40 (64)
+
+# Section 8.17: GPIO Pins Direction Register (IODir)
+# IODir is not available on 740 variant
+
+# Section 8.18: GPIO Pins State Register (IOState)
+# IOState is not available on 740 variant
+
+# Section 8.19: GPIO Interrupt Enable Register (IOIntEna)
+# IOIntEna is not available on 740 variant
+
+# Section 8.20: GPIO Control Register (IOControl)
+IOCONTROL_SOFTWARE_RESET = 0x01 << 3
+IOCONTROL_GPIO_SELECT    = 0x01 << 1 # Not available on 740 variant
+IOCONTROL_LATCH_ENABLE   = 0x01 << 0 # Not available on 740 variant
+
+# Section 8.21: Extra Features Control Register (EFCR)
+EFCR_IRDA_MODE_SLOW = 0x00 << 7
+EFCR_IRDA_MODE_FAST = 0x01 << 7 # Not available on 740 or 750 variant
+EFCR_RTS_INVERT     = 0x01 << 5
+EFCR_RTS_TX_CONTROL = 0x01 << 4
+EFCR_TX_DISABLE     = 0x01 << 2
+EFCR_RX_DISABLE     = 0x01 << 1
+EFCR_RS485_ENABLE   = 0x01 << 0
+
 class SC16IS750:
 
 	def __init__(self, pi, i2cbus = 1, i2caddr = 0x48, xtalfreq = 1843200, baudrate = 115200, databits = LCR_DATABITS_8, stopbits = LCR_STOPBITS_1, parity = LCR_PARITY_NONE):
@@ -180,7 +238,7 @@ class SC16IS750:
 
 	# Reset chip and handle exception thrown by NACK
 	def reset(self):
-		try: self.byte_write_verify(REG_IOCONTROL, 0x01 << 3)
+		try: self.byte_write_verify(REG_IOCONTROL, IOCONTROL_SOFTWARE_RESET)
 		except pigpio.error as e:
 			if e.value == pigpio.error_text(pigpio.PI_I2C_WRITE_FAILED):
 				if self.byte_read(REG_IOCONTROL) != 0x00:
@@ -237,18 +295,18 @@ class SC16IS750:
 	def define_register_set(self, special):
 		return self.enable_register_bit(REG_LCR, 7, special)
 
-	# Write I2C byte to specified register and wait for value to be written
+	# Write I2C byte to specified register
 	def byte_write(self, reg, byte):
 		self.pi.i2c_write_byte_data(self.i2c, self.reg_conv(reg), byte)
 
 	# Read I2C byte from specified register
-	# Return byte received from SMBus
+	# Return byte received from driver
 	def byte_read(self, reg):
 		return self.pi.i2c_read_byte_data(self.i2c, self.reg_conv(reg))
 
 	# Read I2C block from specified register
-	# FIFO is 64 bytes, but SMBus can only read 32 bytes at a time
-	# Return block received from SMBus
+	# FIFO is 64 bytes, but can only read 32 bytes at a time
+	# Return block received from driver
 	def block_read(self, reg, num):
 		block = []
 		while num > 32:

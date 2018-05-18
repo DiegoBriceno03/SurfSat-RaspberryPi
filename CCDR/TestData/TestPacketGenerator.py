244585161d8f4aff0cfd0b79e0dec0d392cd341f
James Phillips <jrphill5@users.noreply.github.com>
2018-05-18 02:16:42 +0000

    Increased packet size to 256 bytes and included write to file

--- CCDR/TestData/TestPacketGenerator.py
+++ CCDR/TestData/TestPacketGenerator.py
@@ -2,40 +2,42 @@ import sys
 import zlib
 import bitstring
 
-#### HEADER ####
-#  2 bits - System Identifications
-#  9 bits - Picoscope Status (<0x140)
-# 52 bits - Data Start Timestamp (integer number of microseconds; can store >48 years)
-#  1 bits - Data Timestamp Units (us/ns)
-# 10 bits - Data Timestamp (in units above)
-#  2 bits - Triggering Channel
-# 20 bits - unused
+### EXAMPLE PICOSCOPE DATA PACKET ###
+########## 256 BYTE PACKET ##########
+#
+#  #######  12 BYTE HEADER #######  
+#  #  2 bits - System Identifications
+#  #  9 bits - Picoscope Status (<0x140)
+#  # 52 bits - Data Start Timestamp (integer number of microseconds; can store >48 years)
+#  #  1 bits - Data Timestamp Units (us/ns)
+#  # 10 bits - Data Timestamp (in units above)
+#  #  2 bits - Triggering Channel
+#  # 20 bits - Unused
+#  ###############################
+#
+#  ## 240 BYTE (60 SAMPLE) BODY ##
+#  #  8 bits - Channel A
+#  #  8 bits - Channel B
+#  #  8 bits - Channel C
+#  #  8 bits - Channel D
+#  ###############################
+#
+#  ####### 4 BYTE CHECKSUM #######
+#  # 32 bits - Checksum
+#  ###############################
+#
+#####################################
 
-# 12 byte header
-
-#### BODY ####
-#  8 bits - Channel A
-#  8 bits - Channel B
-#  8 bits - Channel C
-#  8 bits - Channel D
-
-#  4 byte sample * 28 samples to fill out packet
-
-#### CHECKSUM ####
-# 32 bits - Checksum
-
-#  4 byte checksum
-
-##################
+filename = 'testpacket.pkt'
 
 # Create Bits objects with fake data of correct length 
-sys_ident   = bitstring.Bits('0b00')
-pico_status = bitstring.Bits('0b000000000')
-start_time  = bitstring.Bits('0b0000000000000000000000000000000000000000000000000000')
-time_units  = bitstring.Bits('0b0')
-timestamp   = bitstring.Bits('0b0000000000')
-trigger     = bitstring.Bits('0b00')
-unused      = bitstring.Bits('0b00000000000000000000')
+sys_ident   = bitstring.Bits('0b11')
+pico_status = bitstring.Bits('0b111110110')
+start_time  = bitstring.Bits('0b1110010111010100110000111011001010100010001010110011')
+time_units  = bitstring.Bits('0b1')
+timestamp   = bitstring.Bits('0b1000100110')
+trigger     = bitstring.Bits('0b10')
+unused      = bitstring.Bits('0b10111100110111101111')
 
 # Generate BitStream for header with this fake data
 header = bitstring.BitStream()
@@ -50,7 +52,7 @@ print("Header (%d bytes):\n%s" % (len(header.bytes), header))
 
 # Generate incrementing bytes to act as fake sample values to fill out the packet
 body = bitstring.BitStream()
-for i in range(4*28):
+for i in range(4*60):
 	body.append(bitstring.Bits(hex=('0x%02x' % i)))
 sys.stdout.write("\nBody (%d bytes):" % len(body.bytes))
 for i, v in enumerate(body.bytes):
@@ -69,3 +71,8 @@ for i, v in enumerate(packet.bytes):
 	if i % 16 == 0: sys.stdout.write("\n")
 	sys.stdout.write("%02x" % v)
 sys.stdout.write("\n")
+
+# Write packet out to binary file
+# NOTE: use `xxd testpacket.pkt` on command line to verify output
+with open(filename, 'wb') as fh:
+	fh.write(packet.bytes)

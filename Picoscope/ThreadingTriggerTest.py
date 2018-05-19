fe6b05e983cc5bf9d42fec7a0f43de33b7efe3c1
James Phillips <jrphill5@users.noreply.github.com>
2018-05-19 03:56:30 +0000

    Picoscope now waits for trigger in separate thread

--- Picoscope/ThreadingTriggerTest.py
+++ Picoscope/ThreadingTriggerTest.py
@@ -24,7 +24,7 @@ if status != ps.m.pico_num("PICO_OK"):
 	ps.close_unit()
 	exit(1)
 
-samples = 1024
+samples = 4096
 interval = 4
 htrigr = 0.2
 vtrigr = 0.2
@@ -35,8 +35,6 @@ print("Driver: %s" % ps.info.driver_version)
 print("Firmware: %s/%s" % (ps.info.firmware_version_1, ps.info.firmware_version_2))
 print("ADC Range: %+X/%+X" % (ps.info.min_adc, ps.info.max_adc))
 print("Memory: %X (%.0f MiB)" % (ps.info.memory, ps.info.memory/8.0/1024.0/1024.0))
-
-trigger_event = threading.Event()
 
 channels = ps.m.Channels.map[:ps.info.num_channels] 
 for c in channels:
@@ -99,72 +97,76 @@ print(ps.m.pico_tag(status))
 
 filename = 'data.txt'
 
+def collect():
+	print("Triggering enabled: %s" % ps.is_trigger_set())
+
+	buffers = [None for c in channels]
+	for c in channels:
+		#sys.stdout.write("Locating channel %s buffer ... " % ps.m.Channels.labels[c])
+		#sys.stdout.flush()
+		status, buffers[c] = ps.locate_buffer(channel=c, samples=samples, segment=0, mode=ps.m.RatioModes.raw, downsample=1)
+		#print(ps.m.pico_tag(status))
+
+	sys.stdout.write("Waiting for trigger ... ")
+	sys.stdout.flush()
+	status = ps.collect_segment(segment=0, interval=interval, overlapped=False)
+	print(ps.m.pico_tag(status))
+
+	triggertime = int((time.time()-launchtime)*1e9)
+	print("Trigger time: %X" % triggertime)
+
+	sys.stdout.write("Trigger time offset: ")
+	status, offset = ps.get_trigger_time_offset(segment=0)
+	if offset is not None: print(offset)
+	else: print(ps.m.pico_tag(status))
+
+	sys.stdout.write("Reading data buffers ... ")
+	sys.stdout.flush()
+	data = [None for c in channels]
+	for c in channels:
+		status, info = ps.get_buffer_info(buffers[c])
+		#sys.stdout.write("Reading channel %s buffer with interval %.2fns... " % (ps.m.Channels.labels[c], info["real_interval"]))
+		#sys.stdout.flush()
+		status, buffdata = ps.get_buffer_data(buffers[c])
+		# For non-periodic waveforms, 16-bit data may be represented by 8-bit twos complement integer with no loss
+		data[c] = bytes([((x & 0xFF00) >> 8) for x in buffdata])
+		#print(ps.m.pico_tag(status))
+	print("done!")
+
+	sys.stdout.write("Writing data to file ... ")
+	sys.stdout.flush()
+	with open(filename, "w") as fh:
+		fh.write("System: Picoscope\n")
+		fh.write("Status: 0x%03X\n" % status)
+		fh.write("Timestamp: 0x%015X\n" % triggertime)
+		timestep = info["real_interval"]
+		timeunit = 0
+		if timestep > 1000:
+			timestep /= 1000
+			timeunit += 1
+		fh.write("Timestep Units: 0x%01X\n" % timeunit)
+		fh.write("Timestep: 0x%03X\n" % int(timestep))
+		# Hard coded to say Channel A regardless of triggering channel
+		fh.write("Trigger: 0x%01X\n" % 0)
+		fh.write("Range: 0x%01X\n" % vrange)
+		for c in channels:
+			if c != ps.m.Channels.A: fh.write(", ")
+			fh.write("Channel %s" % ps.m.Channels.labels[c])
+		for sample in zip(*data):
+			fh.write("\n")
+			for i, v in enumerate(sample):
+				if i != 0: fh.write(", ")
+				fh.write("0x%02X" % v)
+		fh.write("\n")
+	print("done!")
+
+t = None
 while True:
 	try:
-
-		print("Triggering enabled: %s" % ps.is_trigger_set())
-
-		buffers = [None for c in channels]
-		for c in channels:
-			#sys.stdout.write("Locating channel %s buffer ... " % ps.m.Channels.labels[c])
-			#sys.stdout.flush()
-			status, buffers[c] = ps.locate_buffer(channel=c, samples=samples, segment=0, mode=ps.m.RatioModes.raw, downsample=1)
-			#print(ps.m.pico_tag(status))
-
-		sys.stdout.write("Waiting for trigger ... ")
-		sys.stdout.flush()
-		status = ps.collect_segment(segment=0, interval=interval, overlapped=False, event_handle=trigger_event)
-		print(ps.m.pico_tag(status))
-
-		triggertime = int((time.time()-launchtime)*1e9)
-		print("Trigger time: %X" % triggertime)
-
-		sys.stdout.write("Trigger time offset: ")
-		status, offset = ps.get_trigger_time_offset(segment=0)
-		if offset is not None: print(offset)
-		else: print(ps.m.pico_tag(status))
-
-		sys.stdout.write("Reading data buffers ... ")
-		sys.stdout.flush()
-		data = [None for c in channels]
-		for c in channels:
-			status, info = ps.get_buffer_info(buffers[c])
-			#sys.stdout.write("Reading channel %s buffer with interval %.2fns... " % (ps.m.Channels.labels[c], info["real_interval"]))
-			#sys.stdout.flush()
-			status, buffdata = ps.get_buffer_data(buffers[c])
-			# For non-periodic waveforms, 16-bit data may be represented by 8-bit twos complement integer with no loss
-			data[c] = bytes([((x & 0xFF00) >> 8) for x in buffdata])
-			#print(ps.m.pico_tag(status))
-		print("done!")
-
-		sys.stdout.write("Writing data to file ... ")
-		sys.stdout.flush()
-		with open(filename, "w") as fh:
-			fh.write("System: Picoscope\n")
-			fh.write("Status: 0x%03X\n" % status)
-			fh.write("Timestamp: 0x%015X\n" % triggertime)
-			timestep = info["real_interval"]
-			timeunit = 0
-			if timestep > 1000:
-				timestep /= 1000
-				timeunit += 1
-			fh.write("Timestep Units: 0x%01X\n" % timeunit)
-			fh.write("Timestep: 0x%03X\n" % int(timestep))
-			# Hard coded to say Channel A regardless of triggering channel
-			fh.write("Trigger: 0x%01X\n" % 0)
-			fh.write("Range: 0x%01X\n" % vrange)
-			for c in channels:
-				if c != ps.m.Channels.A: fh.write(", ")
-				fh.write("Channel %s" % ps.m.Channels.labels[c])
-			for sample in zip(*data):
-				fh.write("\n")
-				for i, v in enumerate(sample):
-					if i != 0: fh.write(", ")
-					fh.write("0x%02X" % v)
-			fh.write("\n")
-		print("done!")
-
-
+		if t is None or not t.is_alive():
+			t = threading.Thread(target=collect)
+			t.start()
+		time.sleep(0.1)
 	except KeyboardInterrupt:
 		break
 

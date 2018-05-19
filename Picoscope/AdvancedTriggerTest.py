79cb1055d6775de85f22b75219d063e39aa13086
James Phillips <jrphill5@users.noreply.github.com>
2018-05-19 01:51:07 +0000

    Modified to output data similar to what will be used on orbit

--- Picoscope/AdvancedTriggerTest.py
+++ Picoscope/AdvancedTriggerTest.py
@@ -1,7 +1,8 @@
-import sys, time
+import sys
+import time
 
 launchtime = time.mktime(time.strptime("2018/01/01 00:00:00", "%Y/%m/%d %H:%M:%S"))
-print("%X" % int(launchtime*1e6))
+print("Launch Timestamp: %015X" % int(launchtime*1e9))
 
 sys.stdout.write("Loading PicoSDK into RAM ... ")
 sys.stdout.flush()
@@ -18,6 +19,8 @@ ps = ps2000a.Device()
 status = ps.open_unit()
 print(ps.m.pico_tag(status))
 
+voltagerange = ps.m.Ranges.r2v
+
 if status != ps.m.pico_num("PICO_OK"):
 	ps.close_unit()
 	exit(1)
@@ -31,7 +34,7 @@ print("Memory: %X (%.0f MiB)" % (ps.info.memory, ps.info.memory/8.0/1024.0/1024.
 channels = ps.m.Channels.map[:ps.info.num_channels] 
 for c in channels:
 	status, state = ps.get_channel_state(channel=c)
-	state.range = ps.m.Ranges.r2v
+	state.range = voltagerange
 	state.enabled = True
 	sys.stdout.write("Enabling channel %s and setting range to %s ... " % (ps.m.Channels.labels[c], ps.m.Ranges.labels[state.range]))
 	sys.stdout.flush()
@@ -92,12 +95,14 @@ sys.stdout.flush()
 status = ps.set_horizontal_trigger_ratio(ratio=htrigr)
 print(ps.m.pico_tag(status))
 
+filename = 'data.txt'
+
 while True:
 	try:
 
 		print("Triggering enabled: %s" % ps.is_trigger_set())
 
-		buffers = dict()
+		buffers = [None for c in channels]
 		for c in channels:
 			#sys.stdout.write("Locating channel %s buffer ... " % ps.m.Channels.labels[c])
 			#sys.stdout.flush()
@@ -109,20 +114,54 @@ while True:
 		status = ps.collect_segment(segment=0, interval=interval, overlapped=False)
 		print(ps.m.pico_tag(status))
 
-		print("Trigger time: %X" % int((time.time()-launchtime)*1e6))
+		triggertime = int((time.time()-launchtime)*1e9)
+		print("Trigger time: %X" % triggertime)
 
 		sys.stdout.write("Trigger time offset: ")
 		status, offset = ps.get_trigger_time_offset(segment=0)
 		if offset is not None: print(offset)
 		else: print(ps.m.pico_tag(status))
 
-		data = dict()
+		sys.stdout.write("Reading data buffers ... ")
+		sys.stdout.flush()
+		data = [None for c in channels]
 		for c in channels:
 			status, info = ps.get_buffer_info(buffers[c])
 			#sys.stdout.write("Reading channel %s buffer with interval %.2fns... " % (ps.m.Channels.labels[c], info["real_interval"]))
 			#sys.stdout.flush()
-			status, data[c] = ps.get_buffer_data(buffers[c])
+			status, buffdata = ps.get_buffer_data(buffers[c])
+			# For non-periodic waveforms, 16-bit data may be represented by 8-bit twos complement integer with no loss
+			data[c] = bytes([((x & 0xFF00) >> 8) for x in buffdata])
 			#print(ps.m.pico_tag(status))
+		print("done!")
+
+		sys.stdout.write("Writing data to file ... ")
+		sys.stdout.flush()
+		with open(filename, "w") as fh:
+			fh.write("System: Picoscope\n")
+			fh.write("Status: 0x%03X\n" % status)
+			fh.write("Timestamp: 0x%015X\n" % triggertime)
+			timestep = info["real_interval"]
+			timeunit = 0
+			if timestep > 1000:
+				timestep /= 1000
+				timeunit += 1
+			fh.write("Timestep Units: 0x%01X\n" % timeunit)
+			fh.write("Timestep: 0x%03X\n" % int(timestep))
+			# Hard coded to say Channel A regardless of triggering channel
+			fh.write("Trigger: 0x%01X\n" % 0)
+			fh.write("Range: 0x%01X\n" % voltagerange)
+			for c in channels:
+				if c != ps.m.Channels.A: fh.write(", ")
+				fh.write("Channel %s" % ps.m.Channels.labels[c])
+			for sample in zip(*data):
+				fh.write("\n")
+				for i, v in enumerate(sample):
+					if i != 0: fh.write(", ")
+					fh.write("0x%02X" % v)
+			fh.write("\n")
+		print("done!")
+
 
 	except KeyboardInterrupt:
 		break
